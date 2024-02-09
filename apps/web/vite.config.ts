c9762f3520d8cab29d219af9d9acce605b652ad7
Gabriel Porto <gabriel_porto@outlook.com>
2024-02-09 18:44:03 -0300

    chore: path for web project

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
+      '@': path.resolve(__dirname, 'src'),
       /* resolve for @repo/ui paths */
       '@ui': path.resolve(__dirname, '../../packages/ui/src'),
     },
