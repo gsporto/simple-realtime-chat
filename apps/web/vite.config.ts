3689ee7b7ca9def17d379e4ffaf5962eb602c139
Gabriel Porto <gabriel_porto@outlook.com>
2024-02-09 18:43:34 -0300

    fix: resolve for @repo/ui

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'node:path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      /* resolve for @repo/ui paths */
+      '@ui': path.resolve(__dirname, '../../packages/ui/src'),
+    },
+  }
 })
