da3ae8204c9b8d2d17b8f28cc6c47279b7f7a28b
Gabriel Porto <gabriel_porto@outlook.com>
2024-02-10 02:36:49 -0300

    fix: postcss error

--- packages/ui/.eslintrc.js
+++ packages/ui/.eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
   root: true,
   extends: ["@repo/eslint-config/react-internal.js"],
   parser: "@typescript-eslint/parser",
+  ignorePatterns: ["postcss.config.cjs"],
   parserOptions: {
     project: "./tsconfig.lint.json",
   },
