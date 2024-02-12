e3e42785ca7c52dcbd6b90b371e5b70588610d91
Gabriel Porto <gabriel_porto@outlook.com>
2024-02-11 23:02:41 -0300

    feat: add message component

---
+++ packages/ui/src/components/ui/message.tsx
@@ -0,0 +1,29 @@
+import { cn } from '@ui/lib/utils';
+import { VariantProps, cva } from 'class-variance-authority';
+
+const messageVariants = cva('border rounded-xl max-w-prose p-2', {
+  variants: {
+    type: {
+      sended: 'self-end rounded-br-none mr-4 ml-14',
+      received: 'rounded-bl-none ml-4 mr-14',
+    },
+  },
+  defaultVariants: {
+    type: 'sended',
+  },
+});
+
+type messageProps = {
+  className?: string;
+  text?: string;
+} & VariantProps<typeof messageVariants>;
+
+function Message({ text, className, type }: messageProps) {
+  return (
+    <div className={cn(messageVariants({ type }), className)}>
+      <p>{text}</p>
+    </div>
+  );
+}
+
+export { Message };
