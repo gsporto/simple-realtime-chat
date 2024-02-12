a503d71614e9408dda7744e60bcf13a0ae9708ba
Gabriel Porto <gabriel_porto@outlook.com>
2024-02-11 23:02:50 -0300

    feat: add messagesMain component

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,11 +1,12 @@
 import { Header } from '@repo/ui/organisms-header';
 import { MessageFooter } from '@repo/ui/messageFooter';
+import { MessagesMain } from '@repo/ui/messagesMain';
 
 function App() {
   return (
     <div className="flex flex-col h-lvh">
       <Header />
-      <div className="flex-1"></div>
+      <MessagesMain />
       <MessageFooter />
     </div>
   );
---
+++ packages/ui/src/components/organisms/messagesMain.tsx
@@ -0,0 +1,32 @@
+import { Message } from '@ui/components/ui/message';
+
+function MessagesMain() {
+  return (
+    <main className="flex flex-col flex-1 space-y-2">
+      <Message
+        type="sended"
+        text="Nunc ut euismod est, at lacinia risus. Nulla rhoncus dignissim odio,
+            non suscipit orci dictum eu. Cras elementum molestie libero non
+            pellentesque."
+      />
+      <Message
+        type="received"
+        text="Nunc ut euismod est, at lacinia risus. Nulla rhoncus dignissim odio,
+        non suscipit orci dictum eu. Cras elementum molestie libero non
+        pellentesque."
+      />
+      <Message
+        type="sended"
+        text="Nunc ut euismod est, at lacinia risus. Nulla rhoncus dignissim odio,
+        non suscipit orci dictum eu. Cras elementum molestie libero non
+        pellentesque. Nunc ut euismod est, at lacinia risus. Nulla rhoncus
+        dignissim odio, non suscipit orci dictum eu. Cras elementum molestie
+        libero non pellentesque. Nunc ut euismod est, at lacinia risus.
+        Nulla rhoncus dignissim odio, non suscipit orci dictum eu. Cras
+        elementum molestie libero non pellentesque."
+      />
+    </main>
+  );
+}
+
+export { MessagesMain };
