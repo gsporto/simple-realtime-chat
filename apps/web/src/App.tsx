af8eb6e78122b1d0717133173cb76be1c9fa742c
Gabriel Porto <gabriel_porto@outlook.com>
2024-02-12 23:01:04 -0300

    feat: add responsive logic

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -6,25 +6,37 @@ import {
   UsersHeader,
 } from './components';
 import { useUserMessage } from './hooks';
+import { useMediaQuery } from 'usehooks-ts';
 
 function App() {
   const { selectedUserId } = useUserMessage();
+  const isLg = useMediaQuery('(min-width: 1024px)');
+
+  const isUsersVisible = isLg || !selectedUserId;
+  const isMessageVisible = isLg || selectedUserId;
+
   return (
     <div className="flex h-lvh">
-      <div className="flex flex-1 flex-col">
-        <UsersHeader />
-        <UserList />
-      </div>
-      <div className="border-l"></div>
-      <div className="flex flex-[2] flex-col">
-        {selectedUserId ? (
-          <>
-            <MessagesHeader />
-            <MessagesMain />
-            <MessagesFooter />
-          </>
-        ) : null}
-      </div>
+      {isUsersVisible && (
+        <div className={'flex flex-1 flex-col'}>
+          <UsersHeader />
+          <UserList />
+        </div>
+      )}
+
+      {isUsersVisible && isMessageVisible && <div className="border-l"></div>}
+
+      {isMessageVisible && (
+        <div className={'flex flex-[2] flex-col'}>
+          {Boolean(selectedUserId) && (
+            <>
+              <MessagesHeader />
+              <MessagesMain />
+              <MessagesFooter />
+            </>
+          )}
+        </div>
+      )}
     </div>
   );
 }
--- apps/web/src/components/organisms/messagesHeader.tsx
+++ apps/web/src/components/organisms/messagesHeader.tsx
@@ -5,22 +5,20 @@ import { ArrowLeftIcon, DotsVerticalIcon } from '@radix-ui/react-icons';
 function MessagesHeader() {
   const { setSelectedUserId, selectedUser } = useUserMessage();
   return (
-    <div>
-      <header className="flex items-center h-16 space-x-3 px-3 border-b">
-        <ArrowLeftIcon
-          className="h-6 w-6"
-          onClick={() => {
-            setSelectedUserId('');
-          }}
-        />
-        <Avatar>
-          <AvatarImage src={selectedUser.image} alt={selectedUser.image} />
-          <AvatarFallback>{selectedUser.name.at(0)}</AvatarFallback>
-        </Avatar>
-        <p className="flex-1">{selectedUser.name}</p>
-        <DotsVerticalIcon className="h-5 w-6" />
-      </header>
-    </div>
+    <header className="flex items-center h-16 space-x-3 px-3 border-b">
+      <ArrowLeftIcon
+        className="h-6 w-6"
+        onClick={() => {
+          setSelectedUserId('');
+        }}
+      />
+      <Avatar>
+        <AvatarImage src={selectedUser.image} alt={selectedUser.image} />
+        <AvatarFallback>{selectedUser.name.at(0)}</AvatarFallback>
+      </Avatar>
+      <p className="flex-1">{selectedUser.name}</p>
+      <DotsVerticalIcon className="h-5 w-6" />
+    </header>
   );
 }
 
