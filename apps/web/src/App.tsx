c11a43ecf905f743b287ba8ab2b5430929538b70
Gabriel Porto <gabriel_porto@outlook.com>
2024-02-13 20:08:59 -0300

    feat: placeholders

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,3 +1,4 @@
+import { ChatBubbleIcon } from '@radix-ui/react-icons';
 import {
   MessagesFooter,
   MessagesHeader,
@@ -29,13 +30,21 @@ function App() {
         {isUsersVisible && isMessageVisible && <div className="border-l"></div>}
 
         {isMessageVisible && (
-          <div className={'flex flex-[2] flex-col'}>
-            {Boolean(targetUserId) && (
+          <div className={'flex flex-1 flex-col 2xl:flex-[2]'}>
+            {targetUserId ? (
               <>
                 <MessagesHeader />
                 <MessagesMain />
                 <MessagesFooter />
               </>
+            ) : (
+              <div className="flex-1 flex flex-col items-center justify-center">
+                <ChatBubbleIcon className="w-60 h-60"></ChatBubbleIcon>
+                <h1 className="text-2xl font-semibold mt-5">Simple Chat App</h1>
+                <blockquote className="mt-6 pl-6 italic">
+                  "Chat 'til you Drop: Where Emoji Speak Louder Than Words!"
+                </blockquote>
+              </div>
             )}
           </div>
         )}
--- apps/web/src/components/organisms/usersList.tsx
+++ apps/web/src/components/organisms/usersList.tsx
@@ -2,26 +2,38 @@ import { User } from '../ui/user';
 import { useUserMessage } from '@/hooks';
 
 function UserList() {
-  const { usersWithMessages: messagesByUser, targetUserId, setTargetUserId } = useUserMessage();
+  const {
+    usersWithMessages: messagesByUser,
+    targetUserId,
+    setTargetUserId,
+  } = useUserMessage();
 
   function handleSelect(id: string) {
     setTargetUserId(id);
   }
 
   return (
-    <div>
-      {messagesByUser.map(user => (
-        <User
-          key={user.id}
-          name={user.name}
-          image={user.image}
-          text={user.messages.at(-1)?.text ?? ''}
-          date="Yesterday"
-          select={targetUserId === user.id}
-          onClick={() => handleSelect(user.id)}
-        />
-      ))}
-    </div>
+    <>
+      {messagesByUser.length ? (
+        <div className="flex-1 overflow-auto">
+          {messagesByUser.map(user => (
+            <User
+              key={user.id}
+              name={user.name}
+              image={user.image}
+              text={user.messages.at(-1)?.text ?? ''}
+              date="Yesterday"
+              select={targetUserId === user.id}
+              onClick={() => handleSelect(user.id)}
+            />
+          ))}
+        </div>
+      ) : (
+        <div className="flex-1 flex justify-center items-center">
+          <p>No users online</p>
+        </div>
+      )}
+    </>
   );
 }
 
