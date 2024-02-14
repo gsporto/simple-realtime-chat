f4eddab755771c21ddb2b2e764b632a497398f13
Gabriel Porto <gabriel_porto@outlook.com>
2024-02-14 16:16:30 -0300

    feat: color scheme

--- apps/web/src/components/organisms/usersHeader.tsx
+++ apps/web/src/components/organisms/usersHeader.tsx
@@ -1,19 +1,74 @@
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { useUserMessage } from '@/hooks';
-import { DotsVerticalIcon } from '@radix-ui/react-icons';
+import {
+  DesktopIcon,
+  DotsVerticalIcon,
+  MoonIcon,
+  SunIcon,
+} from '@radix-ui/react-icons';
 import { Button } from '../ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '../ui/dropdown-menu';
+import { useTheme } from '@/hooks';
+import { Theme } from '@/context';
 
 function UsersHeader() {
   const { currentUser } = useUserMessage();
+  const { theme, setTheme } = useTheme();
+
   return (
     <header className="flex justify-between items-center h-16 space-x-3 px-3 border-b">
       <Avatar>
         <AvatarImage src={currentUser.image} alt={currentUser.name} />
         <AvatarFallback>{currentUser.name.at(0)}</AvatarFallback>
       </Avatar>
-      <Button variant="ghost" size="icon">
-        <DotsVerticalIcon className="h-5 w-6" />
-      </Button>
+
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" size="icon">
+            <DotsVerticalIcon className="h-5 w-6" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end" className="w-48">
+          <DropdownMenuLabel>Color Scheme</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuRadioGroup
+            value={theme}
+            onValueChange={(value) => {
+              setTheme(value as Theme);
+            }}
+          >
+            <DropdownMenuRadioItem
+              value="system"
+              className="flex justify-between"
+            >
+              System
+              <DesktopIcon />
+            </DropdownMenuRadioItem>
+            <DropdownMenuRadioItem
+              value="light"
+              className="flex justify-between"
+            >
+              Light
+              <SunIcon />
+            </DropdownMenuRadioItem>
+            <DropdownMenuRadioItem
+              value="dark"
+              className="flex justify-between"
+            >
+              Dark
+              <MoonIcon />
+            </DropdownMenuRadioItem>
+          </DropdownMenuRadioGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </header>
   );
 }
--- apps/web/src/hooks/index.ts
+++ apps/web/src/hooks/index.ts
@@ -1,1 +1,2 @@
 export * from './useUserMessage';
+export * from './useTheme';
